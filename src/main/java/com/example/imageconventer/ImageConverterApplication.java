2cda57b7de39a09ebd1935b03df16d01f2e1a3bd
nquochuy1995 <nquochuy1995@gmail.com>
2022-09-30 00:05:21 +0700

    firt base source

---
+++ src/main/java/com/example/imageconventer/ImageConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageconventer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageConverterApplication.class, args);
+    }
+
+}
