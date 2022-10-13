4271eb08cda1a45bae71a706c410611e8d5a3b0d
Tung1227 <tungluong2706@gmail.com>
2022-10-13 07:21:38 +0700

    Upload file to folder

--- src/main/java/com/example/imageconventer/controller/UpLoadController.java
+++ src/main/java/com/example/imageconventer/controller/UpLoadController.java
@@ -1,19 +1,32 @@
 package com.example.imageconventer.controller;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class UpLoadController {
 
+    private final String PATH = "C:\\Tung\\nam4\\pbl6\\repo\\python\\image\\";
+
     @ResponseBody
     @PostMapping("/upload")
-    public String upload(MultipartFile multipartFile, Model model) {
-        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-        return "Ok";
+    public String upload(@RequestParam(value="file") MultipartFile file, Model model) throws IOException, JSONException {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        String filePath = PATH + StringUtils.cleanPath(file.getOriginalFilename());
+        file.transferTo(new File(filePath));
+        JSONObject result = new JSONObject();
+        result.put("status", "OK");
+        result.put("name",fileName );
+         return result.toString();
     }
 
 
