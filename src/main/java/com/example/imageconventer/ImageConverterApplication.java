516292688611ec39b4891dcd712bac2549553f01
Tung1227 <tungluong2706@gmail.com>
2022-11-22 22:55:39 +0700

    commit code ok

--- src/main/java/com/example/imageconventer/ImageConverterApplication.java
+++ src/main/java/com/example/imageconventer/ImageConverterApplication.java
@@ -5,15 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ImageConverterApplication extends SpringBootServletInitializer {
-
     public static void main(String[] args) {
         SpringApplication.run(ImageConverterApplication.class, args);
     }
